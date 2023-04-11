24c4b762c96b465f353af8a73953968ff120e648
Pharis Macharia Njoroge <117746016+njorogepharis@users.noreply.github.com>
2023-04-11 13:38:31 +0300

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""the subclass checker"""
+
 
 def inherits_from(obj, a_class):
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
-    return False
+    """returns the subclass yes"""
+    if type(obj) == a_class:
+        return False
+    return issubclass(type(obj), a_class)
