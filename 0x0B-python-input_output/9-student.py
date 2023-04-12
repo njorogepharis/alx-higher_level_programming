0605933ce3503acafb8e47a2745e92ccf0112764
Pharis Macharia Njoroge <117746016+njorogepharis@users.noreply.github.com>
2023-04-12 13:24:57 +0300

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,13 +3,13 @@
 
 
 class Student:
-    """class stuff"""
+    """class items"""
     def __init__(self, first_name, last_name, age):
-        """initializes"""
+        """initialize"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """prints __dict__"""
+        """print the __dict__"""
         return self.__dict__
