60d92b0b0a553b27f60b2397f82ddddb40a75a20
Pharis Macharia Njoroge <117746016+njorogepharis@users.noreply.github.com>
2023-04-12 13:16:40 +0300

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,7 +3,7 @@
 
 
 class Student:
-    """class stuff"""
+    """class items"""
     def __init__(self, first_name, last_name, age):
         """initializes"""
         self.first_name = first_name
@@ -11,7 +11,7 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """prints __dict__"""
+        """print the __dict__"""
         if(attrs and isinstance(attrs, list) and
            all(isinstance(x, str) for x in attrs)):
             return({x: y for x, y in self.__dict__.items() if x in attrs})
@@ -19,6 +19,6 @@ class Student:
             return self.__dict__
 
     def reload_from_json(self, json):
-        """replaces allattributes with json"""
+        """replaces all the attributes with json"""
         if(json):
             self.__dict__ = json
