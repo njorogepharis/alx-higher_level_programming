cbce5fc449a8445a275ed677fa47ba23ad0d60c6
Pharis Macharia Njoroge <117746016+njorogepharis@users.noreply.github.com>
2023-04-12 13:23:13 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""loads adds saves json to file"""
+"""adds saved item in json to file"""
 import json
 import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
